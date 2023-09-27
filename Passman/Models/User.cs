a0b0926434f7b7e2fd228a53a7b19af51946417c
Kristof Zsofenszki <zsofenszki.kristof@gmail.com>
2023-09-27 20:59:20 +0200

    pár teszt

--- Passman/Models/User.cs
+++ Passman/Models/User.cs
@@ -2,6 +2,7 @@ namespace Passman.Models;
 
 public class User
 {
+    public static string? VaultEntryPath { get; set; }
     public string UserId { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
--- Passman/Program.cs
+++ Passman/Program.cs
@@ -6,48 +6,63 @@ namespace Passman
     {
         static void Main(string[] args)
         {
-            // // Példa key és secret
-            // string key = "mySecretKey123";
-            // string secret = "This is a secret message!";
-            //
-            // // Létrehozunk egy példa objektumot
-            // var encryptedType = new EncryptedType();
-            //
-            // // Titkosítás
-            // EncryptedType encryptedData = encryptedType.Encrypt(key, secret);
-            // Console.WriteLine("Titkosított kulcs (key): " + encryptedData.Key);
-            // Console.WriteLine("Titkosított üzenet (secret): " + encryptedData.Secret);
-            //
-            // // Visszafejtés
-            // EncryptedType decryptedData = encryptedType.Decrypt(key, encryptedData.Secret);
-            // Console.WriteLine("Visszafejtett üzenet (secret): " + decryptedData.Secret);
-            
-            if (args.Length > 0)
-            {
-                bool found = false;
-                string command = args[0];
-                if (command == "--register" || command == "-r")
-                {
-                    // Regisztrációs logika
-                    Console.WriteLine("Sikeres regisztráció!");
-                    found = true;
-                }
-
-                if (command == "--login" || command == "-l")
-                {
-                    // Bejelentkezési logika
-                    Console.WriteLine("Sikeres bejelentkezés!");
-                    found = true;
-                }
-                if (!found)
-                {
-                    Console.WriteLine("Ismeretlen parancs. Használat: --register vagy -r");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Nincs parancs megadva. Használat: --register vagy -r");
-            }
+            Console.WriteLine(Add(1,3));   
+        }
+        
+        static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
+
+
+
+
+
+
+
+
+// // Példa key és secret
+// string key = "mySecretKey123";
+// string secret = "This is a secret message!";
+//
+// // Létrehozunk egy példa objektumot
+// var encryptedType = new EncryptedType();
+//
+// // Titkosítás
+// EncryptedType encryptedData = encryptedType.Encrypt(key, secret);
+// Console.WriteLine("Titkosított kulcs (key): " + encryptedData.Key);
+// Console.WriteLine("Titkosított üzenet (secret): " + encryptedData.Secret);
+//
+// // Visszafejtés
+// EncryptedType decryptedData = encryptedType.Decrypt(key, encryptedData.Secret);
+// Console.WriteLine("Visszafejtett üzenet (secret): " + decryptedData.Secret);
+            
+// parancssori argumentumok kezelése
+// if (args.Length > 0)
+// {
+//     bool found = false;
+//     string command = args[0];
+//     if (command == "--register" || command == "-r")
+//     {
+//         // Regisztrációs logika
+//         Console.WriteLine("Sikeres regisztráció!");
+//         found = true;
+//     }
+//
+//     if (command == "--login" || command == "-l")
+//     {
+//         // Bejelentkezési logika
+//         Console.WriteLine("Sikeres bejelentkezés!");
+//         found = true;
+//     }
+//     if (!found)
+//     {
+//         Console.WriteLine("Ismeretlen parancs. Használat: --register vagy -r");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Nincs parancs megadva. Használat: --register vagy -r");
+// }
