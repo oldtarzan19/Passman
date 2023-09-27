5abc60a78909348d3a97f92f0d7feaa0addc5c29
Kristof Zsofenszki <zsofenszki.kristof@gmail.com>
2023-09-27 13:32:29 +0200

    parancssori argumentumok kezelése

--- Passman/Program.cs
+++ Passman/Program.cs
@@ -6,21 +6,48 @@ namespace Passman
     {
         static void Main(string[] args)
         {
-            // Példa key és secret
-            string key = "mySecretKey123";
-            string secret = "This is a secret message!";
+            // // Példa key és secret
+            // string key = "mySecretKey123";
+            // string secret = "This is a secret message!";
+            //
+            // // Létrehozunk egy példa objektumot
+            // var encryptedType = new EncryptedType();
+            //
+            // // Titkosítás
+            // EncryptedType encryptedData = encryptedType.Encrypt(key, secret);
+            // Console.WriteLine("Titkosított kulcs (key): " + encryptedData.Key);
+            // Console.WriteLine("Titkosított üzenet (secret): " + encryptedData.Secret);
+            //
+            // // Visszafejtés
+            // EncryptedType decryptedData = encryptedType.Decrypt(key, encryptedData.Secret);
+            // Console.WriteLine("Visszafejtett üzenet (secret): " + decryptedData.Secret);
+            
+            if (args.Length > 0)
+            {
+                bool found = false;
+                string command = args[0];
+                if (command == "--register" || command == "-r")
+                {
+                    // Regisztrációs logika
+                    Console.WriteLine("Sikeres regisztráció!");
+                    found = true;
+                }
 
-            // Létrehozunk egy példa objektumot
-            var encryptedType = new EncryptedType();
-
-            // Titkosítás
-            EncryptedType encryptedData = encryptedType.Encrypt(key, secret);
-            Console.WriteLine("Titkosított kulcs (key): " + encryptedData.Key);
-            Console.WriteLine("Titkosított üzenet (secret): " + encryptedData.Secret);
-
-            // Visszafejtés
-            EncryptedType decryptedData = encryptedType.Decrypt(key, encryptedData.Secret);
-            Console.WriteLine("Visszafejtett üzenet (secret): " + decryptedData.Secret);
+                if (command == "--login" || command == "-l")
+                {
+                    // Bejelentkezési logika
+                    Console.WriteLine("Sikeres bejelentkezés!");
+                    found = true;
+                }
+                if (!found)
+                {
+                    Console.WriteLine("Ismeretlen parancs. Használat: --register vagy -r");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nincs parancs megadva. Használat: --register vagy -r");
+            }
         }
     }
 }
